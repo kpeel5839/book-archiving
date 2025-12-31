e98da22c80a3a566073bdc8d859fe6962728267f
김재연 <kpeel@gimjaeyeon-ui-MacBookPro.local>
2025-12-31 15:45:14 +0900

    feat: initialize project

---
+++ src/main/kotlin/com/book/Archiving/ArchivingApplication.kt
@@ -0,0 +1,11 @@
+package com.book.Archiving
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArchivingApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArchivingApplication>(*args)
+}
